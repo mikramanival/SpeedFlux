c7c5c8c34f4ed3a7d877bc61f7b7bfd8e471992e
Kim Mark Laviña <mikram@Kims-MacBook-Pro.local>
2026-09-05 23:14:35 +0800

    updated

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from speedflux import data
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('interface', nargs='?', default=None)
-    parser.add_argument('service_id', nargs='?', default=None)
-    parser.add_argument('namespace', nargs='?', default=None)
+    parser.add_parser_argument('--interface', required=True, default=None)
+    parser.add_parser_argument('--service_id', required=False, default=None)
+    parser.add_parser_argument('--namespace', required=True, default=None)
     args = parser.parse_args()
 
     speedflux.initialize()
